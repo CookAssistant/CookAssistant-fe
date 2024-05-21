16605c7556cab8c03c789cac8ed400c0a82abc84
nunomi0 <ykys1@naver.com>
2024-05-21 14:13:32 +0900

    feat : making, community api added

--- lib/ui/page/home/home.dart
+++ lib/ui/page/home/home.dart
@@ -138,7 +138,7 @@ class _HomeScreenState extends State<HomeScreen> {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => RecipeDetailPage(),
+                  builder: (context) => RecipeDetailPage(registered: true),
                 ),
               );
             },
